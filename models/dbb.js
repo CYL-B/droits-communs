b17ca06a1ff82061388c516dc7e5be18d499825a
Camille Benhammadi <benhammadcamille@hotmail.fr>
2022-05-19 07:34:29 +0200

    debug

--- models/dbb.js
+++ models/dbb.js
@@ -2,12 +2,13 @@ var mongoose = require('mongoose');
 
 const REACT_APP_DB = process.env.REACT_APP_DB;
 
+
 var options = {
     connectTimeoutMS: 5000,
     useNewUrlParser: true,
     useUnifiedTopology : true
 }
-mongoose.connect(`mongodb+srv:${REACT_APP_DB}`, 
+mongoose.connect(REACT_APP_DB, 
     options,         
     function(err) {
        console.log(err);
