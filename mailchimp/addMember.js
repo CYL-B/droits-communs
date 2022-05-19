b5f1228adc890556c0442a22b3b00a6157e1d012
Camille Benhammadi <benhammadcamille@hotmail.fr>
2022-05-19 22:08:38 +0200

    heroku

--- mailchimp/addMember.js
+++ mailchimp/addMember.js
@@ -1,18 +1,14 @@
-const dotenv = require("dotenv");
-dotenv.config();
-const REACT_APP_API_CHIMP = process.env.REACT_APP_API_CHIMP;
-
 const mailchimp = require("@mailchimp/mailchimp_marketing");
 const md5 = require("md5")
 
 mailchimp.setConfig({
-  apiKey: REACT_APP_API_CHIMP,
+  apiKey: API_CHIMP,
   server: "us14",
 });
 
 
 async function addMember(email) {
-  console.log(mailchimp)
+  
 
   const hash = md5(email)
     const response = await mailchimp.lists.setListMember(
--- models/dbb.js
+++ models/dbb.js
@@ -1,17 +1,11 @@
 var mongoose = require('mongoose');
 
-const REACT_APP_USER = process.env.REACT_APP_USER;
-const REACT_APP_PASSWORD = process.env.REACT_APP_PASSWORD;
-
-console.log(REACT_APP_USER)
-console.log(REACT_APP_PASSWORD)
-
 var options = {
     connectTimeoutMS: 5000,
     useNewUrlParser: true,
     useUnifiedTopology : true
 }
-mongoose.connect(`mongodb+srv://${REACT_APP_USER}:${REACT_APP_PASSWORD}@cluster0.d5pln.mongodb.net/Common_laws?retryWrites=true&w=majority`, 
+mongoose.connect(DATABASE_URL, 
     options,         
     function(err) {
        console.log(err);
