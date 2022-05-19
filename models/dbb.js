5e2bf87c0ae8bfdf3618ce7ca7cc5915d7a1a447
Camille Benhammadi <benhammadcamille@hotmail.fr>
2022-05-19 18:12:14 +0200

    env

--- models/dbb.js
+++ models/dbb.js
@@ -1,13 +1,14 @@
 var mongoose = require('mongoose');
 
-const REACT_APP_DB = process.env.REACT_APP_DB;
+const REACT_APP_USER = process.env.REACT_APP_USER;
+const REACT_APP_PASSWORD = process.env.REACT_APP_PASSWORD;
 
 var options = {
     connectTimeoutMS: 5000,
     useNewUrlParser: true,
     useUnifiedTopology : true
 }
-mongoose.connect(`mongodb+srv://${REACT_APP_DB}`, 
+mongoose.connect(`mongodb+srv://${REACT_APP_USER}:${REACT_APP_PASSWORD}@cluster0.d5pln.mongodb.net/Common_laws?retryWrites=true&w=majority`, 
     options,         
     function(err) {
        console.log(err);
